56c3fa596d316f8a7d85e6a98a3202bd6e89287f
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-04-12 17:51:12 +0100

    continuing home and learn

---
+++ homeandlearn-js/scripts/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+  // alert(window.navigator.appName)
+  // alert(window.navigator.appVersion)
+  // alert(window.navigator.language)
+  // alert(window.navigator.platform)
+  // alert(window.navigator.userAgent)
+})
