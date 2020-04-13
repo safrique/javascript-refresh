c538fe560db1cabe67e94d280c1f735e93a28bc4
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-04-13 22:05:01 +0100

    added dynamic field to add name to greeting

--- homeandlearn-js/src/scripts/index.js
+++ homeandlearn-js/src/scripts/index.js
@@ -3,11 +3,13 @@ window.addEventListener('load', () => {
   let main_container_div_id = `main_container_div`
   let parts = []
   let menu_id = `menu`
+  let name_added = false
 
   let main_container_div = setMainContainerDiv(main_container_div_id)
   document.body.appendChild(main_container_div)
   main_container_div.appendChild(setH1(`main_h1`))
-
+  main_container_div.appendChild(setGreetingField())
+  setGreetingNameField()
   setElements()
   setMenu()
   setBackGround()
@@ -31,10 +33,11 @@ window.addEventListener('load', () => {
         let part = parts[j]
         let i = document.createElement(`li`)
         i.style.marginBottom = `25px`
+        i.style.color = `white`
         let a = document.createElement(`a`)
         a.innerHTML = part.text
         a.href = `#`
-        a.onclick = () => buildElement(part.id, part.text)
+        a.onclick = () => buildElementForMenu(part.id, part.text)
         a.style.background = `silver`
         a.style.padding = `10px`
         a.style.borderRadius = `5px`
@@ -97,19 +100,17 @@ window.addEventListener('load', () => {
     document.body.style.backgroundRepeat = `no-repeat`
   }
 
-  function buildElement (id, text) {
+  function buildElementForMenu (id, text) {
     removeElement(menu_id)
 
-    if (id === `welcome_popup_div`) {
-      document.getElementById(main_container_div_id).appendChild(setWelcomePopupLink(id, text))
+    switch (id) {
+      case `welcome_popup_div`:
+        document.getElementById(main_container_div_id).appendChild(setWelcomePopupLink(id, text))
+        break
+      default:  // `height_div`
+        document.getElementById(main_container_div_id).appendChild(setScreenHeightDiv(id))
     }
 
-    if (id === `height_div`) {
-      document.getElementById(main_container_div_id).appendChild(setScreenHeightDiv(id))
-    }
-
-    id = `show_menu_button`
-    parts.push({ 'id': id, 'text': `` })
     document.getElementById(main_container_div_id).appendChild(setMenuButton(`show_menu_button`))
   }
 
@@ -145,6 +146,88 @@ window.addEventListener('load', () => {
     d.innerHTML = `Screen Height=` + window.screen.height
     d.style.marginTop = `1em`
     d.style.color = `white`
+    return d
+  }
+
+  function getGreetingText () {
+    let id = `greeting_input`
+    let i = document.getElementById(id)
+
+    if (i !== null) {
+      if (i.value !== ``) {
+        document.getElementById(`greeting_span`).innerHTML = `Hi ${i.value}`
+        removeElement(id)
+        removeElement(`greeting_button`)
+        setGreetingNameField()
+      } else {
+        alert(`Please enter a name to add to the greeting`)
+      }
+    } else {
+      setGreetingNameField()
+      removeElement(`greeting_button`)
+    }
+  }
+
+  function setGreetingNameField () {
+    let d = document.createElement(`d`)
+    d.id = `greeting_field_div`
+    d.style.textAlign = `center`
+    d.style.display = `block`
+    d.style.marginTop = `1em`
+    document.getElementById(`greeting_div`).appendChild(d)
+
+    if (!name_added) {
+      let i = document.createElement(`input`)
+      i.type = `text`
+      i.id = `greeting_input`
+      i.placeholder = `Enter your name`
+      i.style.padding = `0.5em`
+      i.style.borderRadius = `5px`
+      i.style.marginRight = `0.5em`
+      document.getElementById(`greeting_field_div`).appendChild(i)
+
+      document.getElementById(`greeting_span`).innerHTML = `Hi stranger`
+    }
+
+    let b = document.createElement(`button`)
+    b.id = `greeting_button`
+    b.innerHTML = (name_added ? `Change name` : `Add name`)
+    b.onclick = () => getGreetingText()
+    b.style.backgroundColor = `#4CAF50` // Green
+    b.style.border = `true`
+    b.style.borderColor = `black`
+    b.style.color = `white`
+    b.style.padding = `0.5em`
+    b.style.textAlign = `center`
+    b.style.textDecoration = `none`
+    b.style.display = `inline-block`
+    b.style.marginBottom = `2em`
+    b.style.borderRadius = `5px`
+    b.style.cursor = `pointer`
+
+    document.getElementById(`greeting_field_div`).appendChild(b)
+    name_added = !name_added
+  }
+
+  function setGreetingField () {
+    let d = document.createElement(`div`)
+    d.id = `greeting_div`
+    d.style.paddingLeft = `1em`
+    d.style.textAlign = `center`
+    d.style.marginBottom = `0.5em`
+
+    let s = document.createElement(`span`)
+    s.id = `greeting_span`
+    s.style.background = `blue`
+    s.style.color = `white`
+    s.style.padding = `0.5em`
+    s.style.borderRadius = `5px`
+    s.style.border = `true`
+    s.style.borderColor = `black`
+    s.innerHTML = `Hi stranger`
+    s.style.fontSize = `150%`
+    d.appendChild(s)
+
     return d
   }
 })
