c313cd33a33351a43bfa4c608bfbf8d134789c92
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-04-14 16:57:31 +0100

    improved news function

--- homeandlearn-js/src/scripts/news.js
+++ homeandlearn-js/src/scripts/news.js
@@ -5,14 +5,12 @@ function latestNews () {
   let p = document.createElement(`p`)
   p.id = `news_p${inserted}`
 
-  if (!news_added) {
-    p.appendChild(document.createTextNode(`New news goes here`))
-    document.body.insertBefore(p, document.getElementById(`footer`))
-    news_added = true
-  } else {
-    p.appendChild(document.createTextNode(`Some more news...`))
-    document.body.insertBefore(p, document.getElementById(`news_p${inserted - 1}`))
-  }
+  let text = (news_added ? `Some more news...` : `New news goes here`)
+  p.appendChild(document.createTextNode(text))
 
+  let id = (news_added ? `news_p${inserted - 1}` : `footer`)
+  document.body.insertBefore(p, document.getElementById(id))
+
+  news_added = true
   inserted++
 }
